1c229b92a435471a263fadc6f8bab5f84c24c995
Sophia Cheong <sophiaacheong@gmail.com>
2021-05-05 18:11:51 -0700

    Add files via upload

---
+++ src/Bee.js
@@ -0,0 +1,14 @@
+// class Bee {
+//   constructor () {
+//     this.age = 5;
+//   }
+// };
+
+class Bee extends Grub {
+  constructor () {
+    super();
+    this.age = 5;
+    this.color = 'yellow';
+    this.job = 'Keep on growing';
+  }
+};
